e98ad0e1d813f32e1ba623439cac351d76105ed2
GoodNight <3177518021@qq.com>
2024-05-09 01:20:44 +0800

    Update capsulenet.py

--- mixcap/MixerCaps/model/capsnet/capsulenet.py
+++ mixcap/MixerCaps/model/capsnet/capsulenet.py
@@ -4,6 +4,8 @@ from .capsulelayers import *
 from config import cfg
 import torch.nn.init as init
 import math
+import os
+import torch.nn.functional as F
 from einops.layers.torch import Rearrange
 
 
@@ -18,7 +20,7 @@ def squash(inputs, axis=-1):
 
 class Squash(nn.Module):
     """
-    learnable squash(to make length in [0, 1])
+    learnable squash,SASF(to make length in [0, 1])
     Attributes:
     axis: (int) the squash dim
     lmd: (float) the learnable option
@@ -73,42 +75,42 @@ class Affine(nn.Module):
         out = torch.einsum('b c d, o d n -> b o c n', [x, self.w])
         return out
 
-class ATR(nn.Module):
-    """
-    secondary layer of all capsnet
+# class ATR(nn.Module):
+#     """
+#     secondary layer of all capsnet
 
-    Attributes:
-    num: (int) group number
-    in_c: (int) channels of input feature
-    in_dim: (int) dim of intput feature
-    out_dim: (int) dim of output capsules
-    learnable: (bool) if learnable in squash
+#     Attributes:
+#     num: (int) group number
+#     in_c: (int) channels of input feature
+#     in_dim: (int) dim of intput feature
+#     out_dim: (int) dim of output capsules
+#     learnable: (bool) if learnable in squash
 
-    Input:
-    primary capusles (shape like [batch, in_caps_num, in_caps_dim])
+#     Input:
+#     primary capusles (shape like [batch, in_caps_num, in_caps_dim])
 
-    Output:
-    secondary capsules (shape like [batch, num, out_caps_dim])
-    """
-    def __init__(self, num, in_c, in_dim, out_dim, leaxiernable):
-        super().__init__()
+#     Output:
+#     secondary capsules (shape like [batch, num, out_caps_dim])
+#     """
+#     def __init__(self, num, in_c, in_dim, out_dim, leaxiernable):
+#         super().__init__()
 
-        self.in_c = in_c
-        self.squash = Squash(axis=-1, learnable=learnable) # fixme: should be different 2 squash
+#         self.in_c = in_c
+#         self.squash = Squash(axis=-1, learnable=learnable) # fixme: should be different 2 squash
 
-        self.v = Affine(num=num, in_dim=in_dim, out_dim=out_dim)
-        self.mask = nn.Parameter(F.softmax(torch.ones(num, num, self.in_c), dim=0), requires_grad=False)
+#         self.v = Affine(num=num, in_dim=in_dim, out_dim=out_dim)
+#         self.mask = nn.Parameter(F.softmax(torch.ones(num, num, self.in_c), dim=0), requires_grad=False)
 
-    def forward(self, x):
-        v = self.squash(self.v(x))
-        out = torch.einsum('c c n, b c n d -> b c d', [self.mask, v])
+#     def forward(self, x):
+#         v = self.squash(self.v(x))
+#         out = torch.einsum('c c n, b c n d -> b c d', [self.mask, v])
 
-        return self.squash(out)
+#         return self.squash(out)
 
 
 class ATRMixer(nn.Module):
     """
-    Mixer of spatial and feature
+    Mixer of QLR and QDR 
 
     Attributes:
     feature_in_c: (int) in_channels of feature branch
@@ -126,53 +128,45 @@ class ATRMixer(nn.Module):
     Output:
     capsules (shape like [batch, group_num, spatial_out_dim + feature_out_dim])
     """
-    def __init__(self, feature_in_c=2048, spatial_in_c=196, spatial_in_dim=1024, feature_in_dim=49, spatial_out_dim=8, feature_out_dim=8, num=16, learnable=True):
+    def __init__(self, feature_in_c=2048, spatial_in_c=196, spatial_in_dim=1024, feature_in_dim=49, spatial_out_dim=16, feature_out_dim=16, num=16, num_c=64, learnable=True):
         super().__init__()
 
-        self.atr_1 = nn.Sequential(
+        self.atr_lr = nn.Sequential(
             Rearrange('b c h w -> b (h w) c'),
-            ATR(num=num, in_c=spatial_in_c, in_dim=spatial_in_dim, out_dim=spatial_out_dim, learnable=learnable)
+            ATR(num=num, in_c=spatial_in_c, in_dim=spatial_in_dim, num_c=num_c,out_dim=spatial_out_dim, learnable=learnable)
         )
 
-        self.atr_2 = nn.Sequential(
+        self.atr_dr = nn.Sequential(
             Rearrange('b c h w -> b c (h w)'),
             ATR(num=num, in_c=feature_in_c, in_dim=feature_in_dim, out_dim=feature_out_dim, learnable=learnable)
         )
-
+        self.squash_final = Squash(learnable=False)
+        self.fc1 = nn.Linear(16, 1)
+        self.soft = nn.Softmax(dim=1)
     def forward(self, x):
         x1 = x[0]
         x2 = x[1]
-        out2 = self.atr_2(x2)
-        out1 = self.atr_1(x1)
+        
+        out1 = self.atr_lr(x1)
+        out2 = self.atr_dr(x2)
+        
+        # The learnable W for QSM 
+        out_m = self.fc1(out1)  
+        out_m = nn.ReLU()(out_m)
+        w = self.soft(out_m)+1 # w.shape(64,16,1)
+        out1 = torch.mul(out1 , w)
 
+        #Each of the two branches forms a global capsule。
+        out1 = self.fc(out1.view(out1.size(0), -1))
+        out2 = self.fc(out2.view(out2.size(0), -1))
+
+        # mix the capsules from QLR and QDR
         out = torch.cat([out1, out2], dim=2)
-        out = squash(out)
+        out = self.squash_final(out)
+        out = out.norm(dim=-1)
         return out
 
 
-class FC(nn.Module):
-    """
-    Final fully-connected layer
-    
-    Attributes:
-    num: (int) number of capsules
-    dim: (int) dim of capsules
-
-    Input:
-    secondary capsules (shape like [batch, num, dim])
-
-    Output:
-    score (float): the prediction
-    """
-    def __init__(self, num=32, dim=8):
-        super().__init__()
-        self.fc = nn.Linear(num * dim, dim)
-
-    def forward(self, x):
-        x = self.fc(x.view(x.size(0), -1))
-        out = x.norm(dim=-1)
-
-        return out
 
 
 class ContentCaps(nn.Module):
@@ -195,14 +189,14 @@ class ContentCaps(nn.Module):
     Output:
     score (float): the prediction
     """
-    def __init__(self, feature_in_c=2048, spatial_in_c=196, spatial_in_dim=1024, feature_in_dim=49, spatial_out_dim=8, feature_out_dim=8, num=16, learnable=True):
+    def __init__(self, spatial_primary_in_c=1024, feature_primary_in_c=2048, feature_in_c=2048, spatial_in_c=196, spatial_in_dim=1024, feature_in_dim=49, spatial_out_dim=16, feature_out_dim=16, num=16, learnable=True):
         super().__init__()
         self.ce = ATRMixer(feature_in_c=feature_in_c, spatial_in_c=spatial_in_c, spatial_in_dim=spatial_in_dim, feature_in_dim=feature_in_dim, spatial_out_dim=spatial_out_dim, feature_out_dim=feature_out_dim, num=num, learnable=learnable)
 
-        self.fc = FC(num=num, dim=spatial_out_dim + feature_out_dim)
+        #self.fc = FC(num=num, dim=spatial_out_dim + feature_out_dim)
 
     def forward(self, x):
-        x = self.ce(x)
-        x = self.fc(x)
+        out = self.ce(x)
+        #x = self.fc(x)
 
-        return x
+        return out
